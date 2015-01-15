cdbb9a5fffc687684b3ed71d8153a6950b7f7d4f
wkt <thompsow@r1001-lt-100-f.enhnet.org>
2015-01-14 19:18:30 -0600

    u

---
+++ component-brat-annotator/src/test/groovy/clinicalnlp/brat/BratAnnotatorTest.groovy
@@ -0,0 +1,105 @@
+package clinicalnlp.brat;
+
+import static clinicalnlp.dsl.UIMAUtil.*
+import static org.junit.Assert.*
+
+import clinicalnlp.dsl.UIMAUtil
+
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.Level
+import org.apache.uima.analysis_engine.AnalysisEngine
+import org.apache.uima.analysis_engine.AnalysisEngineDescription
+import org.apache.uima.fit.factory.AggregateBuilder
+import org.apache.uima.fit.factory.AnalysisEngineFactory
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory
+import org.apache.uima.jcas.JCas
+import org.cleartk.ne.type.NamedEntityMention
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+import clinicalnlp.type.Relation
+
+import com.google.common.base.Charsets
+import com.google.common.io.Resources
+
+import groovy.util.logging.Log4j
+
+@Log4j
+class BratAnnotatorTest {
+	
+	public static final String GOLD_VIEW = "gold"
+	
+	@BeforeClass
+	public static void setupClass() {
+		BasicConfigurator.configure()
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		log.setLevel(Level.INFO)
+	}
+
+	@Test
+	public void smokeTest() {
+		// test note
+		URL url = Resources.getResource("annotated/path-note-1.txt")
+		String text = Resources.toString(url, Charsets.UTF_8)
+
+		// build a BratAnnotator analysis engine
+		def tsd = TypeSystemDescriptionFactory.createTypeSystemDescription()
+		AnalysisEngineDescription brat = AnalysisEngineFactory.createEngineDescription(
+				BratAnnotatorImpl,
+				BratAnnotator.PARAM_ANN_FILE, "/annotated/path-note-1.ann",
+				BratAnnotator.PARAM_VIEW_NAME, GOLD_VIEW)
+		AggregateBuilder builder = new AggregateBuilder()
+		builder.add(brat)
+		AnalysisEngineDescription desc = builder.createAggregateDescription()
+		PrintWriter writer = new PrintWriter(new File("src/test/resources/descriptors/TestBratUIMAFit.xml"))
+		desc.toXML(writer)
+		writer.close()
+		AnalysisEngine engine = builder.createAggregate()
+		
+		// make the JCas and process
+		JCas jcas = engine.newJCas()
+		jcas.setDocumentText(text)		
+		engine.process(jcas)
+		
+		// test the result
+		Collection<NamedEntityMention> mentions = select(type:NamedEntityMention)
+		assert mentions.size() == 6
+		
+		Collection<Relation> rels = select(type:Relation)
+		assert rels.size() == 3
+	}
+    
+    @Test
+    public void pathNoteWithAttribute() {
+        // build a BratAnnotator analysis engine
+        def tsd = TypeSystemDescriptionFactory.createTypeSystemDescription()
+        AnalysisEngineDescription brat = AnalysisEngineFactory.createEngineDescription(
+                BratAnnotatorImpl,
+                BratAnnotator.PARAM_ANN_FILE, "/annotated/path-note-2.ann",
+                BratAnnotator.PARAM_VIEW_NAME, GOLD_VIEW)
+        AggregateBuilder builder = new AggregateBuilder()
+        builder.add(brat)
+        AnalysisEngineDescription desc = builder.createAggregateDescription()
+        PrintWriter writer = new PrintWriter(new File("src/test/resources/descriptors/TestBratUIMAFit.xml"))
+        desc.toXML(writer)
+        writer.close()
+        AnalysisEngine engine = builder.createAggregate()
+        
+        // make the JCas and process
+        JCas jcas = engine.newJCas()
+        String text = 'foo' * 500
+        jcas.setDocumentText(text)
+        engine.process(jcas)
+        
+        // test the result
+        Collection<NamedEntityMention> mentions = select(type:NamedEntityMention)
+        assert mentions.size() == 2
+        
+        Collection<Relation> rels = select(type:Relation)
+        assert rels.size() == 1
+    }
+}
---
+++ component-brat-annotator/src/test/groovy/clinicalnlp/brat/BratDocumentTest.groovy
@@ -0,0 +1,52 @@
+package clinicalnlp.brat;
+
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertTrue
+import groovy.util.logging.Log4j
+
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.Level
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+import clinicalnlp.brat.AttributeAnnotation;
+import clinicalnlp.brat.BratAnnotation;
+import clinicalnlp.brat.BratDocument;
+import clinicalnlp.brat.RelationAnnotation;
+import clinicalnlp.brat.SpanAnnotation;
+
+@Log4j
+public class BratDocumentTest {
+	static Map<String, String> typeToClassMap = new HashMap<String, String>()
+
+	@BeforeClass
+	public static void setupClass() {
+		BasicConfigurator.configure()
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		log.setLevel(Level.INFO)
+	}
+
+	@Test
+	public void testDocumentParsing() throws IOException {
+		InputStream annIn = BratDocumentTest.class.getResourceAsStream(
+				"/annotated/path-note-1.ann")
+
+		BratDocument doc = BratDocument.parseDocument(annIn)
+
+		Collection<BratAnnotation> anns = doc.getAnnotations()
+		assertEquals(12, anns.size())
+		
+		Map<String, SpanAnnotation> spans = doc.getSpanAnnotations()
+		assertEquals(8, spans.size())
+		
+		Map<String, AttributeAnnotation> attrs = doc.getAttrAnnotations()
+		assertEquals(1, attrs.size())
+		
+		Map<String, RelationAnnotation> rels = doc.getRelAnnotations()
+		assertEquals(3, rels.size())
+	}
+}

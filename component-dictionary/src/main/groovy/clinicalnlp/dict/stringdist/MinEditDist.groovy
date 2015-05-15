3327292931a96c76af8795661c6134cafa86d001
wkt <thompsow@r1001-lt-100-f.enhnet.org>
2015-05-15 08:45:56 -0500

    u

--- component-dictionary/src/main/groovy/clinicalnlp/dict/stringdist/MinEditDist.groovy
+++ component-dictionary/src/main/groovy/clinicalnlp/dict/stringdist/MinEditDist.groovy
@@ -31,19 +31,27 @@ public class MinEditDist implements DynamicStringDist {
 		if (tokens == null) { throw new NullPointerException() }
 		if (tokens.size() == 0) { throw new IllegalArgumentException() }
 		
-		StringJoiner joiner = new StringJoiner(TOKEN_SEP_CHAR)
+		StringBuilder builder = new StringBuilder()
+		builder.append(TOKEN_SEP_CHAR)
 		for (CharSequence token : tokens) {
-			joiner.add(token)
+			builder.append(token)
+			builder.append(TOKEN_SEP_CHAR)
 		}
-		this.text = joiner.toString()
+		this.text = builder.toString()
+		
+//		StringJoiner joiner = new StringJoiner(TOKEN_SEP_CHAR)
+//		for (CharSequence token : tokens) {
+//			joiner.add(token)
+//		}
+//		this.text = joiner.toString()
 		println "Text added: '${this.text}'"
 		
-		BackPtr[] bottomRow = new BackPtr[this.text.length()+1]
-		bottomRow[0] = new BackPtr(score:0, startRowIndex:0)
-		int strIndex = 1
+		BackPtr[] bottomRow = new BackPtr[this.text.length()]
+//		bottomRow[0] = new BackPtr(score:0, startRowIndex:0)
+		int score = 0
 		for (int i = 0; i < text.size(); i++) {
-			if (text[i] == TOKEN_SEP_CHAR) { strIndex = 0 }
-			bottomRow[i+1] = new BackPtr(startRowIndex:i, score:strIndex++)
+			if (text[i] == TOKEN_SEP_CHAR) { score = 0 }
+			bottomRow[i] = new BackPtr(startRowIndex:i, score:score++)
 		}
 		rows.push(bottomRow)
 	}
@@ -53,13 +61,13 @@ public class MinEditDist implements DynamicStringDist {
 		match.append(c)
 		println ("Append: [${match}]")
 		BackPtr[] toprow = rows.peek()
-		BackPtr[] newrow = new BackPtr[toprow.size()]
+		BackPtr[] newrow = new BackPtr[toprow.length]
 		newrow[0] = new BackPtr(score:(toprow[0].score + 1), startRowIndex:0)
 		for (int i = 1; i < newrow.length; i++) {
 			def bptrs = [
-				new BackPtr(startRowIndex:toprow[i-1].startRowIndex, score:(c == text.charAt(i-1) ? toprow[i-1].score : toprow[i-1].score + 1 )),
+				new BackPtr(startRowIndex:toprow[i-1].startRowIndex, score:(c == text.charAt(i) ? toprow[i-1].score : toprow[i-1].score + 1 )),
 				new BackPtr(startRowIndex:toprow[i].startRowIndex, score:(toprow[i].score + 1)),
-				new BackPtr(startRowIndex:toprow[i-1].startRowIndex, score:(newrow[i-1].score + 1))
+				new BackPtr(startRowIndex:newrow[i-1].startRowIndex, score:(newrow[i-1].score + 1))
 				]
 			newrow[i] = GroovyCollections.min(bptrs)
 		}
@@ -75,7 +83,6 @@ public class MinEditDist implements DynamicStringDist {
 	public void removeMatchChar() {
 		if (match.length() == 0) { return }
 		match.deleteCharAt(match.length()-1)
-		assert this.rows.size() > 1
 		this.rows.pop()
 		println ("Remove: [${match}]")
 	}
@@ -85,8 +92,8 @@ public class MinEditDist implements DynamicStringDist {
 		Collection<Integer[]> matches = new ArrayList<>()
 		BackPtr[] toprow = rows.peek()
 		toprow.eachWithIndex { BackPtr bptr, Integer idx ->
-			if (bptr.score <= tolerance && (text.size() <= (idx+1) || text.charAt(idx+1) == TOKEN_SEP_CHAR)) {
-				println "Match found: ${bptr.startRowIndex}, ${idx}; substring: ${text.subSequence(bptr.startRowIndex, idx)}"
+			if (bptr.score <= tolerance && text.charAt(idx+1) == TOKEN_SEP_CHAR) {
+				println "Match found: ${bptr.startRowIndex}, ${idx}; substring: ${text.subSequence(bptr.startRowIndex+1, idx+1)}"
 				matches << ([
 					bptr.startRowIndex,
 					idx

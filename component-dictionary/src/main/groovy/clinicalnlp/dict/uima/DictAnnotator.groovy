6b919ec17e27416d8df86ada41207fcc7be83471
wkt <thompsow@r1001-lt-100-f.enhnet.org>
2015-05-15 15:11:34 -0500

    u

---
+++ component-dictionary/src/main/groovy/clinicalnlp/dict/uima/DictAnnotator.groovy
@@ -0,0 +1,22 @@
+package clinicalnlp.dict.uima
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.resource.ResourceInitializationException;
+
+class DictAnnotator extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void initialize(UimaContext context)
+			throws ResourceInitializationException {
+		super.initialize(context);
+	}
+
+	@Override
+	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		
+	}
+
+}
---
+++ component-dictionary/src/test/groovy/clinicalnlp/dict/uima/DictAnnotatorTest.groovy
@@ -0,0 +1,13 @@
+package clinicalnlp.dict.uima;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class DictAnnotatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
